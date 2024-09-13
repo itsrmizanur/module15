f4f8a7297ae0b1b00faec6e4d9fc846c5536387d
Md Mizanur Rahman <itsrmizanur@gmail.com>
2024-09-13 22:37:13 +0600

    2. Route Handling: done

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const app = express();
 const port = 5500;
 
 
-
+app.get('/', (req, res) => {
+    res.end("This is the Home Page");
+})
+app.get('/about', (req, res) => {
+    res.end("This is the About Page");
+})
+app.get("/contact", (req, res) => {
+    res.end("This is the Contact Page");
+});
 
 
 app.listen(port, () => {
